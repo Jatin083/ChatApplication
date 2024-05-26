a99fc09a913c33e7d9f6e37f55c753981abcc302
jatin <125881431+Jatin083@users.noreply.github.com>
2024-05-26 09:21:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://chatapplication-wkgh.onrender.com',
     credentials:true
 };
 app.use(cors(corsOption)); 
